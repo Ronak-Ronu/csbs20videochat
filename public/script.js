bb2bb06a41630a4effddf45bb7bec86c7a893075
Ronak_Suthar <ronakronu02@gmail.com>
2022-11-19 21:18:50 +0530

    commmit

--- public/script.js
+++ public/script.js
@@ -1,19 +1,11 @@
 const { Server } = require('socket.io');
 const socket=io('/')
-var peerserver=require('peer').ExpressPeerServer;
-var options={
-    debug:true,
-    allow_discovery:true
-};
-let mypeerserver=peerserver(Server,options);
-app.use("/peerjs",mypeerserver)
 const videogrid = document.getElementById('video-grid')
 const mypeer= new Peer(undefined,{
     secure:true,
     host:'csbs20videochat.herokuapp.com',
     port:2002
 })
-
 const myvideo=document.createElement('video')
 myvideo.muted=true
 const peers={}
